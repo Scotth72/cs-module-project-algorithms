f490a6d793cc70366084113673cfb9d94b047b50
Heath Scott <you@example.com>
2020-06-18 14:22:00 -0500

    working on it

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,39 @@
 Input: an integer
 Returns: an integer
 '''
+
+
 def eating_cookies(n):
     # Your code here
+    x = 1
+    y = 0
+    z = 0
 
-    pass
+    for i in range(n):
+        x, y, z = x+y+z, x, y
+    return x
+
+
+# the cache allows us to save our work
+# cache is a dictionary where the keys is the n, value is the answer
+# def eating_cookies(n, cache):
+#     if n < 0:
+#         return 0
+#     elif n == 0:
+#         return 1
+#     # check if the answer is in our cache
+#     elif cache[n] > 0:
+#         return cache[n]
+#     else:
+#         # otherwise, our cache doesn't contain the answer, so wee'll use our
+#         # recursive logic to calculate it, and then save the answer in our
+#         # cache for the future uses
+#         return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,14 +2,19 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr.insert(0, arr.pop(i))
 
-    pass
+    return arr
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
-    arr = [0, 3, 1, 0, -2]
+    arr = [0, 3, 1, 0, -2, 0, 0, 244]
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
